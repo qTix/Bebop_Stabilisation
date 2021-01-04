b90bf23d0468ab8cb5f416a72d5dc4d69f65a5b6
alex <alexandros.sidiras@laposte.net>
2021-01-04 09:13:59 +0100

    Added decollage.py

---
+++ src/decollage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs import Empty
+from std_msgs import UInt8
+from nav_msgs import Odometry
+from sensor_msgs import Image
+import sys, tty, termios
+import numpy as np
+import cv2
+import cv2.cv as cv
+import math
+import struct
+
+def callback(msg):
+    print(msg)
+def callback2(msg):
+    print(msg)
+
+if __name__ == '__main__':
+    rospy.init_node('decollage', anonymous=True)
+    sub = rospy.Subscriber("odom", Odometry, callback)
+    sub_img = rospy.Subscriber("image_raw", Image, callback2)
+    rospy.Publisher("[namespace]/takeoff", Empty, queue_size=10).publish()
+    #flip = rospy.Publisher("[namespace]/flip", UInt8, queue_size=10)
+    #flip.publish(0)
+    rospy.Publisher("[namespace]/land", Empty, queue_size=10).publish()
