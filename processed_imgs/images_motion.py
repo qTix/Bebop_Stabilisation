8915981b6d04e6868144edb2779c6e1ffb8ab13d
mbernard <maxime.bernard@insa-lyon.fr>
2021-01-05 11:48:46 +0100

    Working motion detection (among x and z), still 3 seconds lag from movement to motion detection to fix

--- processed_imgs/images_motion.py
+++ processed_imgs/images_motion.py
@@ -22,6 +22,9 @@ class images_motion(object):
         self.empty_msg = Empty()
         self.bridge = CvBridge()
 
+        self.prev_gray = None
+        self.transforms = []
+
     def callback(self, msg):
         try:
             # Convert your ROS Image message to OpenCV2
@@ -30,57 +33,60 @@ class images_motion(object):
             print(e)
         else:
             curr_gray = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2GRAY)
-            
-            if not self.prev_gray:
+
+            if self.prev_gray is None:
                 curr_gray = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2GRAY)
                 self.prev_gray = curr_gray
+            else:
+                prev_pts = cv2.goodFeaturesToTrack(self.prev_gray,
+                                                 maxCorners=200,
+                                                 qualityLevel=0.01,
+                                                 minDistance=30,
+                                                 blockSize=3)
 
+                # Convert to grayscale
+                gray = cv2.cvtColor(cv2_img,cv2.COLOR_BGR2GRAY)
 
-            prev_pts = cv2.goodFeaturesToTrack(self.prev_gray,20,0.50,20,3)
+                # Calculate optical flow (i.e. track feature points)
+                curr_pts, status, err = cv2.calcOpticalFlowPyrLK(self.prev_gray, curr_gray, prev_pts, None)
 
-            # Convert to grayscale
-            gray = cv2.cvtColor(cv2_img,cv2.COLOR_BGR2GRAY)
+                # Filter only valid points
+                idx = np.where(status==1)[0]
+                prev_pts = prev_pts[idx]
+                curr_pts = curr_pts[idx]
 
-            # Calculate optical flow (i.e. track feature points)
-            curr_pts, status, err = cv2.calcOpticalFlowPyrLK(self.prev_gray, curr_gray, prev_pts, None)
+                #Find transformation matrix
+                m = cv2.estimateRigidTransform(prev_pts, curr_pts, fullAffine=False) #will only work with OpenCV-3 or less
 
-            # Filter only valid points
-            idx = np.where(status==1)[0]
-            prev_pts = prev_pts[idx]
-            curr_pts = curr_pts[idx]
+                # Extract traslation
+                dx = m[0,2]
+                dy = m[1,2]
 
-  #Find transformation matrix
-  m = cv2.estimateRigidTransform(prev_pts, curr_pts, fullAffine=False) #will only work with OpenCV-3 or less
+                # Extract rotation angle
+                da = np.arctan2(m[1,0], m[0,0])
 
-  # Extract traslation
-  dx = m[0,2]
-  dy = m[1,2]
+                # Store transformation
+                self.transforms.append([dx,dy,da])
 
-  # Extract rotation angle
-  da = np.arctan2(m[1,0], m[0,0])
+                # Move to next frame
+                self.prev_gray = curr_gray
 
-  # Store transformation
-  transforms[i] = [dx,dy,da]
+                # print("Tracked points : " + str(len(prev_pts)))
+                print(np.round(self.transforms[-1],1))
 
-  # Move to next frame
-  prev_gray = curr_gray
+                # Visualisation of good goodFeaturesToTrack
+                # for i in self.prev_corners:
+                #     x,y = i.ravel()
+                #     cv2.circle(cv2_img,(x,y),6,255,-1)
+                # time = msg.header.stamp
+                #cv2.imwrite(os.path.join(folder_name,str(time)+'.jpeg'), cv2_img)
 
-  print("Frame: " + str(i) +  "/" + str(n_frames) + " -  Tracked points : " + str(len(prev_pts)))
-
-            self.prev_gray = curr_gray
-
-            # Visualisation of good goodFeaturesToTrack
-            # for i in self.prev_corners:
-            #     x,y = i.ravel()
-            #     cv2.circle(cv2_img,(x,y),6,255,-1)
-            # time = msg.header.stamp
-            #cv2.imwrite(os.path.join(folder_name,str(time)+'.jpeg'), cv2_img)
-
-        try:
-            self.proc_image_pub.publish(self.bridge.cv2_to_imgmsg(cv2_img, "bgr8"))
-        except CvBridgeError as e:
-            print(e)
-        rospy.sleep(0.1)
+                # Publish output image to ROS topic
+                # try:
+                #     self.proc_image_pub.publish(self.bridge.cv2_to_imgmsg(cv2_img, "bgr8"))
+                # except CvBridgeError as e:
+                #     print(e)
+                # rospy.sleep(0.1)
 
     def takeoff(self):
         print("Taking off...")
