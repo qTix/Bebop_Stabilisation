2412751d2b9317f8d4844f67ea29ec0a3386c72e
mbernard <maxime.bernard@insa-lyon.fr>
2021-01-06 16:15:23 +0100

    rm misc

--- misc/teamRed/sift.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import cv2 as cv
-img = cv.imread('home.jpg')
-gray= cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-sift = cv.SIFT_create()
-kp = sift.detect(gray,None)
-img=cv.drawKeypoints(gray,kp, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-cv.imwrite('sift_keypoints.jpg',img)
