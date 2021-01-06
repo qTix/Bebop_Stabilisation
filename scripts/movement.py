9a008f2b34500e9a93ef4d3321d8a6bbced8e35c
unknown <>
2021-01-06 11:15:51 +0100

    modif

--- scripts/movement.py
+++ scripts/movement.py
@@ -10,13 +10,23 @@ def main():
     land = rospy.Publisher('/bebop/land', Empty, queue_size=1)
     move = rospy.Publisher('/bebop/cmd_vel', Twist)
     empty_msg = Empty()
+    move_msg = Twist()
     global i
     rospy.sleep(1)
     takeoff.publish(empty_msg)
     if(i == 0):
-        move.publish('[0.15, 0.0, 0.0]')
+        move_msg.linear.x = 0.15
+        move_msg.linear.y = 0
+        move_msg.linear.z = 0
+        move_msg.angular.z = 0
+        move.publish(move_msg)
+        rospy.sleep(2)
+        move_msg.linear.x = 0
+        move_msg.linear.y = 0
+        move_msg.linear.z = 0
+        move_msg.angular.z = 0
+        move.publish(move_msg)
         rospy.sleep(5)
-        move.publish('[0.0, 0.0, 0.0]')
         i += 1
     if(i == 1):
         land.publish(empty_msg)
